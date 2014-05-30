251637de23a66d20ad969f589c069f6e2954ee71
takoji3 <takoji3g@gmail.com>
2014-05-30 12:53:01 +0900

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,40 @@
-from setuptools import setup
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
 
 setup(
-    name = "movingFace",
-    version = "1.0",
-    packages=['moving_face'],
+    name='MovingFace',
+
+    version='1.0',
+
+    description="A playing module",
+    #long_description=''
+
+    # The project's main homepage.
+    #url='https://github.com/pypa/sampleproject',
+    url=''
+
+    # Author details
+    author='Ryo Kawano',
+    author_email='shonancampus@yahoo.co.jp',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    packages=['moving_face']
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'moving_face=moving_face.core:start',
+        ],
+    },
 )
